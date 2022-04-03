f5ef9bafdfe738521e6b7be3a7e9344e5c52d568
lixu <wflixu@88.com>
2022-04-03 23:12:32 +0800

    v0.0.2

--- packages/use-floating/dist/use-floating.cjs
+++ packages/use-floating/dist/use-floating.cjs
@@ -5,6 +5,42 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var dom = require('@floating-ui/dom');
 var vue = require('vue');
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+  try {
+    var info = gen[key](arg);
+    var value = info.value;
+  } catch (error) {
+    reject(error);
+    return;
+  }
+
+  if (info.done) {
+    resolve(value);
+  } else {
+    Promise.resolve(value).then(_next, _throw);
+  }
+}
+
+function _asyncToGenerator(fn) {
+  return function () {
+    var self = this,
+        args = arguments;
+    return new Promise(function (resolve, reject) {
+      var gen = fn.apply(self, args);
+
+      function _next(value) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+      }
+
+      function _throw(err) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+      }
+
+      _next(undefined);
+    });
+  };
+}
+
 function useFloating(_temp) {
   var _ref = _temp === void 0 ? {
     strategy: 'absolute',
@@ -14,54 +50,75 @@ function useFloating(_temp) {
       placement = _ref.placement,
       strategy = _ref.strategy;
 
-  var reference = vue.ref(null);
-  var floating = vue.ref(null);
+  var reference = vue.ref();
+  var floating = vue.ref();
+
+  var setReference = function setReference(node) {
+    if (reference) {
+      reference.value = node;
+    }
+  };
+
+  var setFloating = function setFloating(node) {
+    if (floating) {
+      floating.value = node;
+    }
+  };
+
   var returnData = vue.reactive({
-    // Setting these to `null` will allow the consumer to determine if
-    // `computePosition()` has run yet
     x: 0,
     y: 0,
     strategy: vue.unref(strategy),
     placement: vue.unref(placement),
-    middlewareData: {}
+    middlewareData: {},
+    reference: setReference,
+    floating: setFloating
   });
 
-  var update = function update() {
-    console.log('update:start', reference.value, floating.value);
+  var update = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+      var data;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              console.log('update:start', reference.value, floating.value);
 
-    if (!reference.value || !floating.value) {
-      return;
-    }
+              if (!(!reference.value || !floating.value)) {
+                _context.next = 3;
+                break;
+              }
 
-    dom.computePosition(reference.value, floating.value, {
-      middleware: middleware,
-      placement: vue.unref(placement),
-      strategy: vue.unref(strategy)
-    }).then(function (data) {
-      console.log(data);
-      Object.assign(returnData, data, {
-        x: vue.ref(data.x),
-        y: vue.ref(data.y)
-      });
-    });
-  }; // useLayoutEffect(update, [update]);
+              return _context.abrupt("return");
 
+            case 3:
+              _context.next = 5;
+              return dom.computePosition(reference.value, floating.value, {
+                middleware: middleware,
+                placement: vue.unref(placement),
+                strategy: vue.unref(strategy)
+              });
 
-  vue.watchEffect(update); // const setReference = (el: Element ):void => {
-  //   reference.value = el;
-  // }
-  // const setFloating = (el: Element ):void => {
-  //   floating.value = el;
-  // }
+            case 5:
+              data = _context.sent;
+              console.log(data);
+              Object.assign(returnData, data);
 
-  return Object.assign(returnData, {
-    refs: {
-      reference: reference,
-      floating: floating
-    },
-    reference: reference,
-    floating: floating
-  });
+            case 8:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function update() {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  vue.watchEffect(update);
+  return returnData;
 }
 var arrow = function arrow(options) {
   var element = options.element,
--- packages/use-floating/dist/use-floating.esm.js
+++ packages/use-floating/dist/use-floating.esm.js
@@ -2,6 +2,42 @@ import { arrow as arrow$1, computePosition } from '@floating-ui/dom';
 export * from '@floating-ui/dom';
 import { ref, reactive, unref, watchEffect, isRef } from 'vue';
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+  try {
+    var info = gen[key](arg);
+    var value = info.value;
+  } catch (error) {
+    reject(error);
+    return;
+  }
+
+  if (info.done) {
+    resolve(value);
+  } else {
+    Promise.resolve(value).then(_next, _throw);
+  }
+}
+
+function _asyncToGenerator(fn) {
+  return function () {
+    var self = this,
+        args = arguments;
+    return new Promise(function (resolve, reject) {
+      var gen = fn.apply(self, args);
+
+      function _next(value) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+      }
+
+      function _throw(err) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+      }
+
+      _next(undefined);
+    });
+  };
+}
+
 function useFloating(_temp) {
   var _ref = _temp === void 0 ? {
     strategy: 'absolute',
@@ -11,54 +47,75 @@ function useFloating(_temp) {
       placement = _ref.placement,
       strategy = _ref.strategy;
 
-  var reference = ref(null);
-  var floating = ref(null);
+  var reference = ref();
+  var floating = ref();
+
+  var setReference = function setReference(node) {
+    if (reference) {
+      reference.value = node;
+    }
+  };
+
+  var setFloating = function setFloating(node) {
+    if (floating) {
+      floating.value = node;
+    }
+  };
+
   var returnData = reactive({
-    // Setting these to `null` will allow the consumer to determine if
-    // `computePosition()` has run yet
     x: 0,
     y: 0,
     strategy: unref(strategy),
     placement: unref(placement),
-    middlewareData: {}
+    middlewareData: {},
+    reference: setReference,
+    floating: setFloating
   });
 
-  var update = function update() {
-    console.log('update:start', reference.value, floating.value);
+  var update = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+      var data;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              console.log('update:start', reference.value, floating.value);
 
-    if (!reference.value || !floating.value) {
-      return;
-    }
+              if (!(!reference.value || !floating.value)) {
+                _context.next = 3;
+                break;
+              }
 
-    computePosition(reference.value, floating.value, {
-      middleware: middleware,
-      placement: unref(placement),
-      strategy: unref(strategy)
-    }).then(function (data) {
-      console.log(data);
-      Object.assign(returnData, data, {
-        x: ref(data.x),
-        y: ref(data.y)
-      });
-    });
-  }; // useLayoutEffect(update, [update]);
+              return _context.abrupt("return");
 
+            case 3:
+              _context.next = 5;
+              return computePosition(reference.value, floating.value, {
+                middleware: middleware,
+                placement: unref(placement),
+                strategy: unref(strategy)
+              });
 
-  watchEffect(update); // const setReference = (el: Element ):void => {
-  //   reference.value = el;
-  // }
-  // const setFloating = (el: Element ):void => {
-  //   floating.value = el;
-  // }
+            case 5:
+              data = _context.sent;
+              console.log(data);
+              Object.assign(returnData, data);
 
-  return Object.assign(returnData, {
-    refs: {
-      reference: reference,
-      floating: floating
-    },
-    reference: reference,
-    floating: floating
-  });
+            case 8:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function update() {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  watchEffect(update);
+  return returnData;
 }
 var arrow = function arrow(options) {
   var element = options.element,
